150960d77f7c8b47d21b56b81aa3787698c0e955
Fran <franciscojavier19961@hotmail.es>
2024-10-03 07:38:54 +0200

    InvertirCadenas

---
+++ InvertirCadenas/src/invertircadenas5/InvertirCadenas5.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package invertircadenas5;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class InvertirCadenas5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+
+        // Lista para almacenar todas las cadenas introducidas
+        ArrayList<String> cadenas = new ArrayList<>();
+
+        String cadena;
+        do {
+            System.out.print("Introduce una cadena (o 'FIN' para terminar): ");
+            cadena = scanner.nextLine();
+
+            //Tanto minuscula como mayuscula
+            if (!cadena.equalsIgnoreCase("FIN")) {
+                cadenas.add(cadena);
+            }
+        } while (!cadena.equalsIgnoreCase("FIN"));
+
+        // Procesar cada cadena y mostrarla invertida
+        System.out.println("\nCadenas invertidas:");
+        for (String c : cadenas) {
+            //¿Como llama a la función del otro ejercicio?
+            System.out.println(invertirCadena(c));
+        }
+
+        // Cerrar el escáner
+        scanner.close();
+    }
+
+  
+    
+}
