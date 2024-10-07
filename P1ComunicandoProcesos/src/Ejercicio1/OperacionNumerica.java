bde2caf8510efb561ea0ac144c824f0ea29f62eb
Claudia Perez <claudissperez99@gmail.com>
2024-10-07 09:32:47 +0200

    Ejercicio1 Practica

---
+++ P1ComunicandoProcesos/src/Ejercicio1/OperacionNumerica.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Ejercicio1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ClaudissPerez
+ */
+public class OperacionNumerica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int number =sc.nextInt();
+        ArrayList<Integer> numbers = new ArrayList<>();
+        int suma =0;
+        
+        while (number!=0){
+            if (number>0){
+                switch (var) {
+                case "":
+                    numbers.add(number);
+                    break;
+                case "-s":
+                    suma +=suma;
+                    break;
+                case "-m":
+                    suma +=suma;
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+            }
+            Scanner sc = new Scanner(System.in);
+            int numero =sc.nextInt();
+        }
+        
+    }
+    
+}
