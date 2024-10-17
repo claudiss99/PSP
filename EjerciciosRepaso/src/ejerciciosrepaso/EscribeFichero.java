e454193ddf9a7ab6d0a4c9ad36b34ad3828b9411
Fran <franciscojavier19961@hotmail.es>
2024-10-17 11:17:28 +0200

    5

--- EjerciciosRepaso/src/ejerciciosrepaso/EscribeFichero.java
+++ EjerciciosRepaso/src/ejerciciosrepaso/EscribeFichero.java
@@ -4,13 +4,35 @@
  */
 package ejerciciosrepaso;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  *
  * @author Claudia
  */
 public class EscribeFichero {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.err.println("No se ha encontrado fichero");
+            System.exit(1);
+        }else{
+            String fileName = args[0];
+            Scanner sc = new Scanner(System.in);
+            //Se lee la linea
+            String linea = sc.nextLine();
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName,true))){
+                while (linea.equalsIgnoreCase("fin")){
+                    writer.write(linea);
+                    writer.newLine();
+                    linea=sc.nextLine();
+                }
+                writer.flush();
+                writer.close();
+            }
+        }
     }
           
 }
--- EjerciciosRepaso/src/ejerciciosrepaso/LeeFichero.java
+++ EjerciciosRepaso/src/ejerciciosrepaso/LeeFichero.java
@@ -4,12 +4,28 @@
  */
 package ejerciciosrepaso;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author Claudia
  */
 public class LeeFichero {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        if(args.length == 0){
+            System.err.println("No se ha encontrado fichero");
+            System.exit(1);
+        }else{
+            String fileName = args[0];
+            String linea = "";
+            try (BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+                while ((linea = reader.readLine())!= null){
+                    System.out.println(linea);
+                }
+            }
+        }
     }
 }
