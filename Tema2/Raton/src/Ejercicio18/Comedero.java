8234b3c204346fb9e48fe76b3e57c79647cc814f
Claudia <claudissperez99@gmail.com>
2024-11-25 12:49:19 +0100

    hacienod

---
+++ Tema2/Raton/src/Ejercicio18/Comedero.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ModEj15;
+
+/**
+ *
+ * @author ClaudissPerez
+ */
+public class Comedero {
+    private int comidaDisponible;
+
+    public Comedero(int comidaDisponible) {
+        this.comidaDisponible = comidaDisponible;
+    }
+
+    public int getComidaDisponible() {
+        return comidaDisponible;
+    }
+
+    public void setComidaDisponible(int comidaDisponible) {
+        this.comidaDisponible = comidaDisponible;
+    }
+    
+    
+}
